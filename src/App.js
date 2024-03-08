5fbd9b5e573835be6d1cc25477376c442ff2b03d
mlionhart <lionrhart@gmail.com>
2024-03-08 08:57:36 -0700

    fixed fetch urls to heroku

--- src/App.js
+++ src/App.js
@@ -29,7 +29,9 @@ function App() {
 
   // Fetch tasks
   const fetchTasks = async () => {
-    const res = await fetch("http://localhost:5000/tasks");
+    const res = await fetch(
+      "https://frozen-river-11954-4fdc380c8747.herokuapp.com/tasks"
+    );
     const data = await res.json();
 
     return data;
@@ -37,7 +39,9 @@ function App() {
 
   // Fetch task
   const fetchTask = async (id) => {
-    const res = await fetch(`http://localhost:5000/tasks/${id}`);
+    const res = await fetch(
+      `https://frozen-river-11954-4fdc380c8747.herokuapp.com/tasks/${id}`
+    );
     const data = await res.json();
 
     return data;
@@ -45,13 +49,16 @@ function App() {
 
   // Add Task
   const addTask = async (task) => {
-    const res = await fetch("http://localhost:5000/tasks", {
-      method: "POST",
-      headers: {
-        "Content-type": "application/json",
-      },
-      body: JSON.stringify(task),
-    });
+    const res = await fetch(
+      "https://frozen-river-11954-4fdc380c8747.herokuapp.com/tasks",
+      {
+        method: "POST",
+        headers: {
+          "Content-type": "application/json",
+        },
+        body: JSON.stringify(task),
+      }
+    );
 
     // data is just new task that's added. Need await because it returns a promise
     const data = await res.json();
@@ -67,9 +74,12 @@ function App() {
 
   // Delete Task
   const deleteTask = async (id) => {
-    await fetch(`http://localhost:5000/tasks/${id}`, {
-      method: "DELETE",
-    });
+    await fetch(
+      `https://frozen-river-11954-4fdc380c8747.herokuapp.com/tasks/${id}`,
+      {
+        method: "DELETE",
+      }
+    );
 
     setTasks(tasks.filter((task) => task.id !== id));
   };
@@ -79,13 +89,16 @@ function App() {
     const taskToToggle = await fetchTask(id)
     const updTask = { ...taskToToggle, reminder: !taskToToggle.reminder }
 
-    const res = await fetch(`http://localhost:5000/tasks/${id}`, {
-      method: 'PUT',
-      headers: {
-        'Content-type': 'application/json'
-      },
-      body: JSON.stringify(updTask)
-    })
+    const res = await fetch(
+      `https://frozen-river-11954-4fdc380c8747.herokuapp.com/tasks/${id}`,
+      {
+        method: "PUT",
+        headers: {
+          "Content-type": "application/json",
+        },
+        body: JSON.stringify(updTask),
+      }
+    );
 
     // data is updated task
     const data = await res.json()
